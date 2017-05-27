1de7eb8fbc8bde034aff000dc2fee287733b92a3
Kārlis Repsons <karlis.repsons@gmail.com>
2017-05-27 21:18:05 +0300

    SquareContainer changes: * add onMounted prop, the related code and notes.
    * API documentation: add .resize().
    * restructured to have two private variables.
    * simplify export statements a bit.

--- source/Reusables/UI/Containers/SquareContainer/SquareContainer.jsx
+++ source/Reusables/UI/Containers/SquareContainer/SquareContainer.jsx
@@ -9,7 +9,12 @@ Usage:
 Pre-defined container class names:
     centered - to center content square both vertically and horizontally.
 
+SquareContainer
+    .resize()
+
 Notes:
+    * This component first mounts its container, measures it and
+      only then it mounts contents and attempts to call onMounted.
     * SquareContainerExample component is also exported.
 */
 
@@ -17,6 +22,7 @@ import PropTypes from 'prop-types';
 const propTypes = {
     className: PropTypes.string, // add class names to container div as 'name1 name2'
     children: PropTypes.node,
+    onMounted: PropTypes.func, // called when content square is mounted in the DOM
     onResize: PropTypes.func // f(newContentSideLength)
 };
 
@@ -26,54 +32,67 @@ import React from 'react';
 
 import './SquareContainer.scss';
 
-class SquareContainer extends React.Component {
+export default class SquareContainer extends React.Component {
     constructor() {
         super();
         this.state = { sideLength: 0 };
-    }
-    resize() {
-        if (this.areaFiller) {
-            const rect = this.areaFiller.getBoundingClientRect();
-            const newSideLength = Math.min(rect.width, rect.height);
-            this.setState({ sideLength: newSideLength });
-            if (typeof this.props.onResize === 'function')
-                this.props.onResize(newSideLength);
-        }
-    }
-    componentDidMount() {
-        this.resize();
-        window.addEventListener('resize', this.resize.bind(this));
-    }
-    componentWillUnmount() {
-        window.removeEventListener('resize', this.resize.bind(this));
-    }
-    render() {
-        const L = this.state.sideLength;
-        let content = null;
-        if (L >= Number.EPSILON)
-            content =
-                <div className='content'
-                    style={{ width: `${L}px`, height: `${L}px` }}
-                >
-                    {this.props.children}
-                </div>;
-        
-        return (
-            // div.fill-all-area is needed to avoid overcomplicated 
-            // process of getting precise element's content width and height.
-            // this.areaFiller should be private.
-            <div className={`square container kU7d2 ${this.props.className}`}>
-                <div className='fill-all-area'
-                    ref={r => this.areaFiller = r}
-                >
-                    {content}
-                </div>
-            </div>
-        );
+
+        let isMounted = false;
+        let areaFiller = null;
+        Object.assign(this, {
+            resize() {
+                if (areaFiller) {
+                    const rect = areaFiller.getBoundingClientRect();
+                    const newSideLength = Math.min(rect.width, rect.height);
+                    this.setState({ sideLength: newSideLength });
+                    if (typeof this.props.onResize === 'function')
+                        this.props.onResize(newSideLength);
+                }
+            },
+            componentDidMount() {
+                this.resize();
+                window.addEventListener('resize', this.resize.bind(this));
+            },
+            componentWillUnmount() {
+                isMounted = false
+                // now, can another div.content ref call follow before re-mount?
+                ; 
+                window.removeEventListener('resize', this.resize.bind(this));
+            },
+            render() {
+                const L = this.state.sideLength;
+                let content = null;
+                if (L >= Number.EPSILON)
+                    content =
+                        <div className='content'
+                            ref={() => {
+                                if (!isMounted && this.props.onMounted)
+                                    this.props.onMounted();
+                                isMounted = true;
+                            }}
+                            style={{ width: `${L}px`, height: `${L}px` }}
+                        >
+                            {this.props.children}
+                        </div>;
+
+                return (
+                    // div.fill-all-area is needed to avoid overcomplicated 
+                    // process to get precise element's content dimensions.
+                    <div className={'square container kU7d2 '
+                        + `${this.props.className}`}
+                    >
+                        <div className='fill-all-area'
+                            ref={r => areaFiller = r}
+                        >
+                            {content}
+                        </div>
+                    </div>
+                );
+            }
+        });
     }
 }
 
-export default SquareContainer;
 export { SquareContainer };
 export { SquareContainerExample } from './Example';
 
