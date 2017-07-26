2901400909257825980bf3e21abf627d620a3d9a
Kārlis Repsons <karlis.repsons@gmail.com>
2017-07-26 20:50:43 +0300

    Function areNumbersDifferent improved: * check if arguments are numbers (throw exception).

--- source/reusables/math/jsfloat/compare/areNumbersDifferent.js
+++ source/reusables/math/jsfloat/compare/areNumbersDifferent.js
@@ -1,3 +1,15 @@
+import {ArgumentException} from 'exceptionTypes_mjS3d';
+
 export default function areNumbersDifferent(n1, n2) {
-    return Math.abs(n1 - n2) >= Number.EPSILON;
+   if (typeof n1 !== 'number' || typeof n2 !== 'number') {
+      const arg1Text = typeof n1 !== 'number' ?
+         ' For first parameter.' : '';
+      const arg2Text = typeof n1 !== 'number' ?
+         ' For second parameter.' : '';
+      
+      throw new ArgumentException(
+         `Got non-number input.${arg1Text}${arg2Text}`);
+   }
+   
+   return Math.abs(n1 - n2) >= Number.EPSILON;
 }
